2cd0fe76b7cc43204cace7ce7c79d5be752c06fe
umarabid123 <umarabid709@gmail.com>
2025-12-02 00:31:10 +0500

    changes in discover page

--- src/screens/DiscoverScreen.tsx
+++ src/screens/DiscoverScreen.tsx
@@ -1,100 +1,171 @@
-import React from "react";
+import React from 'react';
 import {
   Dimensions,
   FlatList,
   StyleSheet,
   View,
   ScrollView,
-} from "react-native";
-import CarouselItem from "../components/CarouselItem";
-import Header from "../components/common/Header";
-import SearchSection from "../components/common/SearchSection";
-import { Colors } from "../constant";
-import AppText from "../components/common/AppText";
-import EventCard from "../components/EventCard";
+} from 'react-native';
+import CarouselItem from '../components/CarouselItem';
+import Header from '../components/common/Header';
+import SearchSection from '../components/common/SearchSection';
+import { Colors } from '../constant';
+import AppText from '../components/common/AppText';
+import EventCard from '../components/EventCard';
+import FighterCard from '../components/FighterCard';
+import AppButton from '../components/common/AppButton';
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 export default function DiscoverScreen() {
   const data = [
-    { id: "1", image: require("../../assets/images/caraousal-icon.png") },
-    { id: "2", image: require("../../assets/images/caraousal-icon.png") },
-    { id: "3", image: require("../../assets/images/caraousal-icon.png") },
-    { id: "4", image: require("../../assets/images/caraousal-icon.png") },
+    { id: '1', image: require('../../assets/images/caraousal-icon.png') },
+    { id: '2', image: require('../../assets/images/caraousal-icon.png') },
+    { id: '3', image: require('../../assets/images/caraousal-icon.png') },
+    { id: '4', image: require('../../assets/images/caraousal-icon.png') },
   ];
 
   const events = [
     {
+      id: '1',
+      title: 'Muay Thai Showdown',
+      subtitle: 'Bangkok Arena • 24 Nov',
+      image: require('../../assets/images/event-card-img.png'),
+    },
+    {
+      id: '2',
+      title: 'Championship Night',
+      subtitle: 'London Stadium • 12 Dec',
+      image: require('../../assets/images/event-card-img.png'),
+    },
+    {
+      id: '3',
+      title: 'Glory Clash',
+      subtitle: 'Dubai Dome • 29 Dec',
+      image: require('../../assets/images/event-card-img.png'),
+    },
+    {
+      id: '4',
+      title: 'Glory Clash',
+      subtitle: 'Dubai Dome • 29 Dec',
+      image: require('../../assets/images/event-card-img.png'),
+    },
+  ];
+
+  const fighters = [
+    {
       id: "1",
-      title: "Muay Thai Showdown",
-      subtitle: "Bangkok Arena • 24 Nov",
-      image: require("../../assets/images/event-card-img.png"),
+      fighterName: "Jaspar Landal",
+      fighterImage: require("../../assets/images/event-card-img.png"),
+      fighterFlag: require("../../assets/images/flag-icon.png"),
+      discipline: "Muay Thai",
+      fightRecord: "12-4-0",
+      weightClass: "63.5 kg",
     },
     {
       id: "2",
-      title: "Championship Night",
-      subtitle: "London Stadium • 12 Dec",
-      image: require("../../assets/images/event-card-img.png"),
+      fighterName: "Ethan Carter",
+      fighterImage: require("../../assets/images/event-card-img.png"),
+      fighterFlag: require("../../assets/images/flag-icon.png"),
+      discipline: "Kickboxing",
+      fightRecord: "10-2-1",
+      weightClass: "70 kg",
     },
     {
       id: "3",
-      title: "Glory Clash",
-      subtitle: "Dubai Dome • 29 Dec",
-      image: require("../../assets/images/event-card-img.png"),
+      fighterName: "Leo Marshall",
+      fighterImage: require("../../assets/images/event-card-img.png"),
+      fighterFlag: require("../../assets/images/flag-icon.png"),
+      discipline: "Boxing",
+      fightRecord: "15-1-0",
+      weightClass: "75 kg",
     },
   ];
 
   return (
     <View style={styles.container}>
       <Header />
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <SearchSection />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <SearchSection />
 
-      <View style={{ height: 220 }}>
-        <FlatList
-          data={data}
-          horizontal
-          scrollEnabled={true}      // 👈 important (ScrollView handles scrolling)
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(item) => item.id}
-          contentContainerStyle={{ paddingHorizontal: 20, marginTop: 40 }}
-          ItemSeparatorComponent={() => <View style={{ width: 8 }} />}
-          renderItem={({ item }) => (
-            <CarouselItem
-              imageSource={item.image}
-              size={173}
-              borderRadius={16}
-            />
-          )}
-        />
-      </View>
+        <View style={{ height: 220 }}>
+          <FlatList
+            data={data}
+            horizontal
+            scrollEnabled={true} // 👈 important (ScrollView handles scrolling)
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => item.id}
+            contentContainerStyle={{ paddingHorizontal: 20, marginTop: 40 }}
+            ItemSeparatorComponent={() => <View style={{ width: 8 }} />}
+            renderItem={({ item }) => (
+              <CarouselItem
+                imageSource={item.image}
+                size={173}
+                borderRadius={16}
+              />
+            )}
+          />
+        </View>
 
-      <View style={{ paddingHorizontal: 20, paddingTop: 30 }}>
-        <AppText
-          text="Events soon 👀"
-          fontSize={16}
-          color={"#FFFFFFCC"}
-          style={{ fontWeight: 500 }}
-        />
+        <View style={{ paddingHorizontal: 20, paddingTop: 30, paddingBottom:40 }}>
+          <AppText
+            text="Events soon 👀"
+            fontSize={16}
+            color={'#FFFFFFCC'}
+            style={{ fontWeight: 500 }}
+          />
 
-        <FlatList
-          data={events}
-          keyExtractor={(item) => item.id}
-          scrollEnabled={false}      // 👈 also disable
-          contentContainerStyle={{ paddingBottom: 50, paddingTop: 16 }}
-          ItemSeparatorComponent={() => <View style={{ height: 14 }} />}
-          renderItem={({ item }) => (
-            <EventCard
-              title={item.title}
-              subtitle={item.subtitle}
-              image={item.image}
-              containerStyle={styles.cardSpacing}
-              onPress={() => console.log("Pressed:", item.title)}
-            />
-          )}
-        />
-      </View>
-    </ScrollView></View>
+          <FlatList
+            data={events}
+            keyExtractor={item => item.id}
+            scrollEnabled={false} 
+            contentContainerStyle={{paddingTop: 16 }}
+            ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
+            renderItem={({ item }) => (
+              <EventCard
+                title={item.title}
+                subtitle={item.subtitle}
+                image={item.image}
+                containerStyle={styles.cardSpacing}
+                onPress={() => console.log('Pressed:', item.title)}
+              />
+            )}
+          />
+          <AppButton text='Browse events' btnStyle={{backgroundColor:'transparant', borderWidth:1, borderColor:"#FFFFFF80", borderRadius:50, width:"auto", paddingHorizontal:32, paddingVertical:17}} />
+        </View>
+
+        <View style={{ paddingHorizontal: 20 }}>
+          <AppText
+            text="Popular fighters 🔥"
+            fontSize={16}
+            color={'#FFFFFFCC'}
+            style={{ fontWeight: 500 }}
+          />
+
+          <FlatList
+            data={fighters}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={item => item.id}
+            contentContainerStyle={{
+              paddingTop: 20,
+              paddingHorizontal: 20,
+            }}
+            ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+            renderItem={({ item }) => (
+              <FighterCard
+                fighterName={item.fighterName}
+                fighterImage={item.fighterImage}
+                fighterFlag={item.fighterFlag}
+                discipline={item.discipline}
+                fightRecord={item.fightRecord}
+                weightClass={item.weightClass}
+              />
+            )}
+          />
+            <AppButton text='Browse fighters' btnStyle={{backgroundColor:'transparant', borderWidth:1, borderColor:"#FFFFFF80", borderRadius:50, width:"auto", paddingHorizontal:32, paddingVertical:17}} />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
